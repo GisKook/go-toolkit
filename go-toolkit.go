08a4bb1cebca67c9e5bb7305674ec687b7f0e03d
zhangkai <zhangkai.gis@163.com>
2015-07-15 09:36:05 +0800

    int2byte byte2int

---
+++ go-toolkit.go
@@ -0,0 +1,36 @@
+package gktoolkit
+
+import (
+	"encoding/binary"
+	"log"
+	"runtime"
+)
+
+func Trace() bool {
+	pc, fn, line, _ := runtime.Caller(1)
+	log.Printf("[trace] in %s[%s:%d] \n", runtime.FuncForPC(pc).Name(), fn, line)
+
+	return true
+}
+
+func Int64ToBytes(i int64) []byte {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, uint64(i))
+
+	return buf
+}
+
+func BytesToInt64(buf []byte) int64 {
+	return int64(binary.BigEndian.Uint64(buf))
+}
+
+func Int32ToBytes(i int32) []byte {
+	var buf = make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, uint32(i))
+
+	return buf
+}
+
+func BytesToInt32(buf []byte) int32 {
+	return int32(binary.BigEndian.Uint32(buf))
+}
